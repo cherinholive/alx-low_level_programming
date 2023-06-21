0be46b44180847c24015ea4fed303c5e3b80467a
root <cherinhokendrick@gmail.com>
2023-06-21 12:00:09 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - print the sign of input number
  * @n: input value which is int
- * Return 1,0,-1 due on the input
+ * Return: 1,0,-1 due on the input
  */
 
 int print_sign(int n)
@@ -15,10 +15,11 @@ int print_sign(int n)
 	} else if (n < 0)
 	{
 		_putchar('-');
-                return (-1);
+		return (-1);
 	} else
 	{
 		_putchar('0');
-                return (0);
+		return (0);
 	}
 }
+
