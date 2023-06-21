14a712e924acb1ed7dfa0bf49df5a5fa0dc3b2c3
root <cherinhokendrick@gmail.com>
2023-06-21 11:56:01 -0700

    4 update

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of input number
+ * @n: input value which is int
+ * Return 1,0,-1 due on the input
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n < 0)
+	{
+		_putchar('-');
+                return (-1);
+	} else
+	{
+		_putchar('0');
+                return (0);
