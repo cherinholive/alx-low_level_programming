e280eff5d57f403ad76bf24531bfb5c56dcbfc45
root <cherinhokendrick@gmail.com>
2023-06-21 11:04:52 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *
+ * main - list a-z
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		_putchar(l);
+		_putchar('\n');
+	}
+}
+
