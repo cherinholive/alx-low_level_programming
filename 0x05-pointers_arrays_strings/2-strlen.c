f4b9326b3135dd2585d5d8dafde30bd030c0ccae
root <cherinhokendrick@gmail.com>
2023-06-27 10:26:33 -0700

    0-5

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,16 @@
-a
+#include "main.h"
+
+/*
+ * _strlen - return len of string
+ * @s: string input
+ * Return: lenght of s
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s[len] != '\0')
+		len++
+	return(len);
+}
