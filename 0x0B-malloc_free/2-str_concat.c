3f1271671a26790b353c89328df7d87565d0b484
root <cherinhokendrick@gmail.com>
2023-07-11 16:28:59 -0700

    0x0B ehhn

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,37 +11,34 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int l1, l2, i, k;
-	char *result;
+	char *strout;
+	unsigned int i, j, k, limit;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
 
-	for (l1 = 0; s1[l1] != '\0'; l1++)
-		;
-	for (l2 = 0; s2[l2] != '\0'; l2++)
+	for (i = 0; s1[i] != '\0'; i++)
 		;
 
-	result = (char *)malloc((l1 + l2 + 1) * sizeof(char));
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
-	if (result == NULL)
+	strout = malloc(sizeof(char) * (i + j + 1));
+
+	if (strout == NULL)
 	{
-		free(result);
+		free(strout);
 		return (NULL);
 	}
 
-	for (i = 0; i <= l1; i++)
-	{
-		result[i] = s1[i];
-	}
+	for (k = 0; k < i; k++)
+		strout[k] = s1[k];
 
-	for (k = 0; k <= l2; i++, k++)
-	{
-		result[i] = s2[k];
-	}
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		strout[k] = s2[j];
 
-	return (result);
+	return (strout);
 }
