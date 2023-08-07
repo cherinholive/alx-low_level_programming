5acc75e461ad5991a1f4182bb9a6482f3a38179e
root <cherinhokendrick@gmail.com>
2023-08-07 07:08:05 -0700

    0x15 task 3'

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and 
+ * read_textfile - reads a text file and
  * prints it to the POSIX standard output.
  * @filename: file pointer
  * @letter: number of letter to print.
@@ -35,5 +35,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	free(buf);
 
-	return (num_letter_output);	
+	return (num_letter_output);
 }
