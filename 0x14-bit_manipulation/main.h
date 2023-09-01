d6f075352a9d3d9c026e006a9dd94616985c11bf
chernor <cherinhokendrick@gmail.com>
2023-09-01 06:58:51 -0700

    chernor: main.h update

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,17 @@
+#ifndef _main_h_
+#define _main_h_
+
+#include <stdlib.h>
+
+int cal_power(int base, int power);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+int _putchar(char c);
+
+#endif
