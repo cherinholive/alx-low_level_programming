f40c9175fe5d7bc8b43381e00d6b5ed8cd08c4ba
chernor <chernihokendrick@gmail.com>
2023-09-29 08:26:28 -0700

    0-1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,60 +1,17 @@
-#include <stdio.h>
+#ifndef _main_h_
+#define _main_h_
+
+#include <stdlib.h>
 
 int cal_power(int base, int power);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+int _putchar(char c);
 
-int cal_power(int base, int power)
-{
-	int result = 1;
-	
-	while (power != 0)
-	{
-		result *= base;
-       		power--;
-	}
-
-	return result;
-}
-
-unsigned int binary_to_uint(const char *b)
-{
-	int base, power, i, j, len;
-	unsigned int result = 0;
-
-	base = 2;
-
-	if (b == NULL)
-		return (0);
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (j = 0; b[j] != '\0'; j++)
-	{
-		i = 0;
-		power = len - 1;
-		i = cal_power(base, power);
-		i = i * (int)b[j];
-		result += i;
-		len--;
-	}
-
-	return (result);
-}
-
-int main(void)
-{
-    unsigned int n;
-
-    n = binary_to_uint("1");
-    printf("%u\n", n);
-    n = binary_to_uint("101");
-    printf("%u\n", n);
-    n = binary_to_uint("1e01");
-    printf("%u\n", n);
-    n = binary_to_uint("1100010");
-    printf("%u\n", n);
-    n = binary_to_uint("0000000000000000000110010010");
-    printf("%u\n", n);
-    return (0);
-}
+#endif
